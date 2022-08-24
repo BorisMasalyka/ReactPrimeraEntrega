f8274cb445ac5fcaa9e0d61684a7a22a9de67fe3
BorisMasalyka <104439268+BorisMasalyka@users.noreply.github.com>
2022-08-23 21:17:45 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <p>
+        Hola, esta es la primera entrega del curso de React.js
+      </p>
+    </div>
+
+  );
+}
+
+export default App;
